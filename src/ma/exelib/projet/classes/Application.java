d0482a6be0ca8e67f9dcaf5e73c110d8bf83ed52
HornBee <antonin.sfre@hotmail.fr>
2019-06-27 15:56:48 +0200

    correctif

--- src/ma/exelib/projet/classes/Application.java
+++ src/ma/exelib/projet/classes/Application.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ma.exelib.projet.classes;
 
 public class Application {
@@ -6,8 +5,8 @@ public class Application {
 	public static void main(String[] args) {
 
 		Personne[] personnes = new Personne[6];
-		personnes[0]= new Etudiant ("Durant","Clement", " Bougarel sur iseudre");
-		personnes[1]= new Etudiant ("Durand","Marc", " Bougarel sur iseudre");
+		//personnes[0]= new Etudiant ("Durant","Clement", " Bougarel sur iseudre");
+		//personnes[1]= new Etudiant ("Durand","Marc", " Bougarel sur iseudre");
 		personnes[2]= new Employe ("DEEDD","Julius",  1000);
 		personnes[3]= new Employe ("AZAZS","Jerome",  8000);
 		personnes[4]= new Professeur ("DEEDD","Julius", 5, "Bio");
@@ -22,28 +21,3 @@ public class Application {
 }
 
 //un commentaire pour tester github
-=======
-package ma.exelib.projet.classes;
-
-public class Application {
-
-	public static void main(String[] args) {
-
-		Personne[] personnes = new Personne[6];
-	//	personnes[0]= new Etudiant ("Durant","Clement", " Bougarel sur iseudre");
-	//	personnes[1]= new Etudiant ("Durand","Marc", " Bougarel sur iseudre");
-		personnes[2]= new Employe ("DEEDD","Julius",  1000);
-		personnes[3]= new Employe ("AZAZS","Jerome",  8000);
-		personnes[4]= new Professeur ("DEEDD","Julius", 5, "Bio");
-		personnes[5]= new Professeur ("DEEDD","JCIzeur",1.5, "Poterie");
-
-		for (Personne p : personnes) {
-			System.out.println(p);
-		}
-
-	}
-
-}
-
-//un commentaire pour tester github
->>>>>>> branch 'BranchAnto' of https://github.com/lacompaniecreole/IOTA.git
