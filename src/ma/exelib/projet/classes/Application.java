af3fd03eaa5e606f4ac301f06a1b655dcbbbbb03
ib <ib@WINIB-7F3M0TDKM>
2019-06-27 15:57:50 +0200

    commit

--- src/ma/exelib/projet/classes/Application.java
+++ src/ma/exelib/projet/classes/Application.java
@@ -5,7 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 
 		Personne[] personnes = new Personne[6];
-	//	personnes[0]= new Etudiant ("Durant","Clement", " Bougarel sur iseudre");
+	///	personnes[0]= new Etudiant ("Durant","Clement", " Bougarel sur iseudre");
 	///	personnes[1]= new Etudiant ("Durand","Marc", " Bougarel sur iseudre");
 		personnes[2]= new Employe ("DEEDD","Julius",  1000);
 		personnes[3]= new Employe ("AZAZS","Jerome",  8000);
