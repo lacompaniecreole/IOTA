981bf6268517cff303934f81ab3020fc5c01cc6b
ib <ib@WINIB-7F3M0TDKM>
2019-06-27 15:44:15 +0200

    ced com

--- src/ma/exelib/projet/classes/Application.java
+++ src/ma/exelib/projet/classes/Application.java
@@ -5,8 +5,8 @@ public class Application {
 	public static void main(String[] args) {
 
 		Personne[] personnes = new Personne[6];
-		personnes[0]= new Etudiant ("Durant","Clement", " Bougarel sur iseudre");
-		personnes[1]= new Etudiant ("Durand","Marc", " Bougarel sur iseudre");
+	//	personnes[0]= new Etudiant ("Durant","Clement", " Bougarel sur iseudre");
+	//	personnes[1]= new Etudiant ("Durand","Marc", " Bougarel sur iseudre");
 		personnes[2]= new Employe ("DEEDD","Julius",  1000);
 		personnes[3]= new Employe ("AZAZS","Jerome",  8000);
 		personnes[4]= new Professeur ("DEEDD","Julius", 5, "Bio");
